0e5f2dbec608aec10057778947b16e19d997b79c
anagordon <anbgordon@gmail.com>
2025-01-20 17:32:22 -0500

    Update auth.py

--- project_new/trial_something/auth.py
+++ project_new/trial_something/auth.py
@@ -711,10 +711,9 @@ def identify():
 
             # Define the specific string you're looking for
             specific_string = Medication.upper()
-            # print(df.columns)
-            df.columns = df.columns.str.strip()
+            print(df.columns)
             # Filter rows where the DRUGNAME column contains the specific string
-            filtered_df = df[df['DRUGNAME'].str.contains(specific_string, case=False, na=False)]
+            # filtered_df = df[df['DRUGNAME'].str.contains(specific_string, case=False, na=False)]
 
             # ---- GET AGE INFORMATION -----
             # Define age bins and labels for grouping
