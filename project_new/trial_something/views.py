3d1f07207e95d41ac4a4b489bb0738f76d1eb3f5
Ana Gordon <143830954+anagordon@users.noreply.github.com>
2025-04-11 10:16:35 -0400

    Update views.py

--- project_new/trial_something/views.py
+++ project_new/trial_something/views.py
@@ -170,84 +170,84 @@ def get_study(studies_related_pair_input):
 
     return num_studies, female_proportion, male_proportion, tot_num_females, tot_num_males
 
-# def get_model(drug, disease):
-#     # model_file_path = r'C:\Users\anbgo\coding_projects_flask\GitHub-Rx\project_new\trial_something\regression_model.pkl'
-#     # preprocessor_file_path =  r'C:\Users\anbgo\coding_projects_flask\GitHub-Rx\project_new\trial_something\preprocessor.pkl'
+def get_model(drug, disease):
+    # model_file_path = r'C:\Users\anbgo\coding_projects_flask\GitHub-Rx\project_new\trial_something\regression_model.pkl'
+    # preprocessor_file_path =  r'C:\Users\anbgo\coding_projects_flask\GitHub-Rx\project_new\trial_something\preprocessor.pkl'
 
-#     model_file_path = os.path.join(os.path.dirname(__file__), 'regression_model.pkl')
-#     preprocessor_file_path = os.path.join(os.path.dirname(__file__), 'preprocessor.pkl')
+    model_file_path = os.path.join(os.path.dirname(__file__), 'regression_model.pkl')
+    preprocessor_file_path = os.path.join(os.path.dirname(__file__), 'preprocessor.pkl')
     
-#     BASE_DIR1 = os.path.dirname(os.path.abspath(__file__))  # Get the directory of the current file
-#     csv_file1 = os.path.join(BASE_DIR1, 'ctg-studies.csv')  # Join the base directory with the file name
-#     df1 = pd.read_csv(csv_file1)  
+    BASE_DIR1 = os.path.dirname(os.path.abspath(__file__))  # Get the directory of the current file
+    csv_file1 = os.path.join(BASE_DIR1, 'ctg-studies.csv')  # Join the base directory with the file name
+    df1 = pd.read_csv(csv_file1)  
 
-#     #cleaning names
-#     df1["Conditions"] = df1["Conditions"].str.upper()
-#     df1["Interventions"] = df1["Interventions"].str.upper()
-#     df1["Conditions"] = df1["Conditions"].fillna('').astype(str)
-#     df1["Interventions"] = df1["Interventions"].fillna('').astype(str)
+    #cleaning names
+    df1["Conditions"] = df1["Conditions"].str.upper()
+    df1["Interventions"] = df1["Interventions"].str.upper()
+    df1["Conditions"] = df1["Conditions"].fillna('').astype(str)
+    df1["Interventions"] = df1["Interventions"].fillna('').astype(str)
 
  
-#     # Load processor
-#     with open(preprocessor_file_path, 'rb') as f:
-#         preprocessor = pickle.load(f)
+    # Load processor
+    with open(preprocessor_file_path, 'rb') as f:
+        preprocessor = pickle.load(f)
 
-#     # Load model
-#     with open(model_file_path, 'rb') as f:
-#         model = pickle.load(f)
+    # Load model
+    with open(model_file_path, 'rb') as f:
+        model = pickle.load(f)
 
-#     nct_list = get_study_ids(drug, disease, df1)
-#     num_studies, female_proportion, male_proportion, tot_num_females, tot_num_males = get_study(nct_list)
+    nct_list = get_study_ids(drug, disease, df1)
+    num_studies, female_proportion, male_proportion, tot_num_females, tot_num_males = get_study(nct_list)
 
-#     (top_3, total_participants, sum_females, sum_males, female_proportion1, male_proportion1) = sort(nct_list)
+    (top_3, total_participants, sum_females, sum_males, female_proportion1, male_proportion1) = sort(nct_list)
 
-#     sample_data = pd.DataFrame({
-#         'Indication': [disease],
-#         'Num Studies': [num_studies],
-#         'Total females in studies': [tot_num_females],
-#         'Total males in studies': [tot_num_males],
-#         'Male proportion in studies': [male_proportion],
-#         'Female proportion in studies': [female_proportion],
-#         'Number of participants in most relevant studies': [total_participants],
-#         'Number of female participants in most relevant studies': [sum_females],
-#         'Number of male participants in most relevant studies': [sum_males],
-#         'Proportion of females in most relevant studies': [female_proportion1],
-#         'Proportion of males in most relevant studies': [male_proportion1]
-#     })
+    sample_data = pd.DataFrame({
+        'Indication': [disease],
+        'Num Studies': [num_studies],
+        'Total females in studies': [tot_num_females],
+        'Total males in studies': [tot_num_males],
+        'Male proportion in studies': [male_proportion],
+        'Female proportion in studies': [female_proportion],
+        'Number of participants in most relevant studies': [total_participants],
+        'Number of female participants in most relevant studies': [sum_females],
+        'Number of male participants in most relevant studies': [sum_males],
+        'Proportion of females in most relevant studies': [female_proportion1],
+        'Proportion of males in most relevant studies': [male_proportion1]
+    })
 
-#     transformed_data = preprocessor.transform(sample_data)
+    transformed_data = preprocessor.transform(sample_data)
 
-#     transformed_columns = (
-#         preprocessor.transformers_[0][1].get_feature_names_out(['Indication']).tolist() +
-#         [
-#             # 'Indication',
-#             # 'Num Studies',
-#             # 'Total females in studies',
-#             # 'Total males in studies',
-#             # 'Male proportion in studies',
-#             # 'Female proportion in studies',
-#             # 'Number of participants in most relevant studies',
-#             # 'Number of female participants in most relevant studies',
-#             # 'Number of male participants in most relevant studies',
-#             # 'Proportion of females in most relevant studies',
-#             # 'Proportion of males in most relevant studies'
-#             'Total females in studies', 
-#             'Total males in studies',
-#             'Female proportion in studies', 
-#             'Male proportion in studies',
-#             'Proportion of females in most relevant studies', 
-#             'Proportion of males in most relevant studies',
-#             'Num Studies',
-#             'Number of participants in most relevant studies', 
-#             'Number of female participants in most relevant studies',
-#             'Number of male participants in most relevant studies'
-#         ]
-#     )
+    transformed_columns = (
+        preprocessor.transformers_[0][1].get_feature_names_out(['Indication']).tolist() +
+        [
+            # 'Indication',
+            # 'Num Studies',
+            # 'Total females in studies',
+            # 'Total males in studies',
+            # 'Male proportion in studies',
+            # 'Female proportion in studies',
+            # 'Number of participants in most relevant studies',
+            # 'Number of female participants in most relevant studies',
+            # 'Number of male participants in most relevant studies',
+            # 'Proportion of females in most relevant studies',
+            # 'Proportion of males in most relevant studies'
+            'Total females in studies', 
+            'Total males in studies',
+            'Female proportion in studies', 
+            'Male proportion in studies',
+            'Proportion of females in most relevant studies', 
+            'Proportion of males in most relevant studies',
+            'Num Studies',
+            'Number of participants in most relevant studies', 
+            'Number of female participants in most relevant studies',
+            'Number of male participants in most relevant studies'
+        ]
+    )
 
-#     transformed_df = pd.DataFrame(transformed_data, columns=transformed_columns)
-#     transformed_df.drop(columns=['Female proportion in studies','Male proportion in studies', 'Proportion of females in most relevant studies', 'Proportion of males in most relevant studies' ],  inplace=True)
-#     prediction = model.predict(transformed_df)*100
-#     return np.around(prediction[0], 2) 
+    transformed_df = pd.DataFrame(transformed_data, columns=transformed_columns)
+    transformed_df.drop(columns=['Female proportion in studies','Male proportion in studies', 'Proportion of females in most relevant studies', 'Proportion of males in most relevant studies' ],  inplace=True)
+    prediction = model.predict(transformed_df)*100
+    return np.around(prediction[0], 2) 
 
 
 @views.route('/calculate-risk', methods=['GET', 'POST'])
