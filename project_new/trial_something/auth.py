47f6ba6dfb101ae69af9839acec932656d63d124
Ana Gordon <143830954+anagordon@users.noreply.github.com>
2025-04-11 10:11:02 -0400

    Update auth.py

--- project_new/trial_something/auth.py
+++ project_new/trial_something/auth.py
@@ -31,7 +31,8 @@ from albumentations import (
     Rotate, ShiftScaleRotate, Transpose
 )
 from albumentations.pytorch import ToTensorV2
-from .views import get_model
+# from trial_something.views import get_model
+from .utils import get_model
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 import openai
