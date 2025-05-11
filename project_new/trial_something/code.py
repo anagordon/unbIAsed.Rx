4e3063aae136f26b7c9ea51f5f8230aed56ecf03
anagordon <anbgordon@gmail.com>
2025-05-11 13:14:40 -0400

    Update code.py

--- project_new/trial_something/code.py
+++ project_new/trial_something/code.py
@@ -219,9 +219,18 @@ def new_model():
         # model = BertForSequenceClassification.from_pretrained('bert-base-uncased', output_hidden_states=True)
 
         model_str = "NeuML/pubmedbert-base-embeddings"
-        tokenizer = AutoTokenizer.from_pretrained(model_str)
-        model = AutoModel.from_pretrained(model_str)
-        print("Model and tokenizer loaded successfully.")
+        # tokenizer = AutoTokenizer.from_pretrained(model_str)
+        # model = AutoModel.from_pretrained(model_str)
+        # print("Model and tokenizer loaded successfully.")
+        try:
+            print("Loading tokenizer...")
+            tokenizer = AutoTokenizer.from_pretrained(model_str)
+            print("Loading model...")
+            model = AutoModel.from_pretrained(model_str)
+            print("Model and tokenizer loaded successfully.")
+        except Exception as e:
+            print(f"Error loading model or tokenizer: {e}")
+            return "An error occurred while loading the model or tokenizer.", 500
 
         # Function to get sentence embeddings
         def get_sentence_embedding(sentences):
