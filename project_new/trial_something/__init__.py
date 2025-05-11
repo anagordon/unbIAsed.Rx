8e68799703ff7e9728183ac2d59688e2fc8a196e
anagordon <anbgordon@gmail.com>
2025-05-10 21:56:18 -0400

    ediots for website

--- project_new/trial_something/__init__.py
+++ project_new/trial_something/__init__.py
@@ -22,7 +22,7 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-    app.register_blueprint(code, url_prefix='/identify')
+    app.register_blueprint(code, url_prefix='/')
 
     from .models import User, Note, Drugs, Info
     
