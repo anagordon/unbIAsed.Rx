2fdc70e2b6c3f9c54f5bc925f39e635ab394e733
anagordon <anbgordon@gmail.com>
2025-05-11 22:45:25 -0400

    Update code.py

--- project_new/trial_something/code.py
+++ project_new/trial_something/code.py
@@ -258,10 +258,11 @@ def new_model():
         # Load FAISS index and metadata
         index_file_path = os.path.join(os.path.dirname(__file__), 'merged_index_19000.index')
         index = faiss.read_index(index_file_path)
+        print("FAISS index loaded successfully.")
 
         metadata_file_path = os.path.join(os.path.dirname(__file__), 'merged_metadata_19000.csv')
         metadata_df = pd.read_csv(metadata_file_path)
-
+        print("Metadata loaded successfully.")
         # Function to query the FAISS database - UPDATED VIN'S VERSION november 21st
         def query_database(query_sentence, index_path, metadata_path, top_k=10):
             # Load the metadata
